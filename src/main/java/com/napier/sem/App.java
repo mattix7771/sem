eaaaa096ca50450815ac8b7d1beef6ada007d0ed
mattix7771 <mattione7@gmail.com>
2022-02-24 00:46:18 +0000

    Support for specific employee salary

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -14,12 +14,17 @@ public class App
         a.connect();
 
         // Extract employee salary information
-        ArrayList<Employee> employees = a.getAllSalaries();
+        //ArrayList<Employee> employees = a.getAllSalaries();
+
+        String title = "Engineer";
+
+        // Extract specific employee salary information
+        ArrayList<Employee> employees = a.getSpecificSalaries(title);
 
         // Test the size of the returned data - should be 240124
         System.out.println(employees.size());
 
-        //Print out salaries
+        // Print out salaries
         printSalaries(employees);
 
         // Disconnect from database
@@ -75,6 +80,7 @@ public class App
         }
     }
 
+
     /**
      * Gets all the current employees and salaries.
      * @return A list of all employees and salaries, or null if there is an error.
@@ -90,6 +96,50 @@ public class App
                     "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary "
                             + "FROM employees, salaries "
                             + "WHERE employees.emp_no = salaries.emp_no AND salaries.to_date = '9999-01-01' "
+                            + "ORDER BY employees.emp_no ASC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<Employee> employees = new ArrayList<Employee>();
+            while (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("employees.emp_no");
+                emp.first_name = rset.getString("employees.first_name");
+                emp.last_name = rset.getString("employees.last_name");
+                emp.salary = rset.getInt("salaries.salary");
+                employees.add(emp);
+            }
+            return employees;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salary details");
+            return null;
+        }
+    }
+
+
+    /**
+     * Gets current employees and salaries based on title.
+     * @return A list of all employees and salaries, or null if there is an error.
+     */
+    public ArrayList<Employee> getSpecificSalaries(String title)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary "
+                            + "FROM employees, salaries, titles "
+                            + "WHERE employees.emp_no = salaries.emp_no\n" +
+                            "            AND employees.emp_no = titles.emp_no\n" +
+                            "            AND salaries.to_date = '9999-01-01'\n" +
+                            "            AND titles.to_date = '9999-01-01'\n" +
+                            "            AND titles.title = '<title>' "
                             + "ORDER BY employees.emp_no ASC";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
