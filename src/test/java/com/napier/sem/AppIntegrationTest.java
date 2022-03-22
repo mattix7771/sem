7735364c77301818954eec92ac0801f2954c3097
mattix7771 <mattione7@gmail.com>
2022-03-22 18:47:01 +0000

    Added addEmployee

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -55,4 +55,18 @@ public class AppIntegrationTest
         ArrayList<Employee> salaries = app.getSpecificSalaries("None");
         assertEquals(salaries.size(), 0);
     }
+
+    @Test
+    void testAddEmployee()
+    {
+        Employee emp = new Employee();
+        emp.emp_no = 500000;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        app.addEmployee(emp);
+        emp = app.getEmployee(500000);
+        assertEquals(emp.emp_no, 500000);
+        assertEquals(emp.first_name, "Kevin");
+        assertEquals(emp.last_name, "Chalmers");
+    }
 }
